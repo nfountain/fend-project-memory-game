580fe82f66d881139dda5d630b50d5c7a1d2cbdf
Nicena Fontaine <nicenas.email@gmail.com>
2018-07-08 17:45:41 -0600

    Fix typos and clean up code.

--- js/app.js
+++ js/app.js
@@ -6,17 +6,43 @@ const deckOfCards = document.querySelector('.deck');
 let card = document.querySelectorAll('.card');
 let cardsThatMatch = document.querySelectorAll('.match');
 let listOfCards = [...card];
-let flippedCards = []; // Idea to create an empty array to hold the flipped cards is courtesy of Matt Cranford's blog, accessed 06/19/18 at <https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/>
+let flippedCards = [];
+/* Idea to create an empty array to hold the flipped cards is courtesy of Matt
+Cranford's blog, accessed 06/19/18 at
+<https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/> */
 let matchedCards = [];
 
 // Buttons (reset game, and restart game from the winGame modal)
 const resetButton = document.querySelector('.restart');
 const restartButton = document.querySelector('.new-game');
 
+// Moves (Counter)
+const moveCount = document.querySelector('.moves');
+let moves = 0;
+
+// Stars
+let stars = document.querySelectorAll('ul.stars li');
+let starList = ['star1', 'star2', 'star3'];
+
+// Clock/Timer
+const clock = document.querySelector('.clock');
+let seconds = 0;
+let minutes = 0;
+let timer;
+let timerOff = true;
+
+// Modal and its components
+const modal = document.querySelector('.win-modal');
+let winTime = document.querySelector('.clock-win');
+let winMoves = document.querySelector('.moves-win');
+let winStars = document.querySelector('.stars-win');
+
+
 // INTERACTING WITH THE CARDS AND DECK
-// Resources: Card shuffle function came pre-loaded by Udacity; originally from <http://stackoverflow.com/a/2450976>
+/* Resources: Card shuffle function came pre-loaded by Udacity; originally from
+<http://stackoverflow.com/a/2450976> */
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -35,7 +61,10 @@ function resetCards() {
         deckOfCards.innerHTML = "";
         [].forEach.call(listOfCards, function(newCard) {
             deckOfCards.appendChild(newCard);
-        });// Using an empty array to call the forEach method is courtesy of sandraisreal accessed 06/24/18, at <https://github.com/sandraisrael/Memory-Game-fend>
+        });
+        /* Using an empty array to call the forEach method is courtesy of
+        sandraisreal accessed 06/24/18, at 
+        <https://github.com/sandraisrael/Memory-Game-fend> */
         listOfCards[i].classList.remove('match', 'open', 'show');
     };
 };
@@ -46,7 +75,10 @@ function flipCard(evtTarget) {
     evtTarget.classList.toggle('show');
 };
 
-// Resources: firstElementChild.classList did not work, so used Matt Cranford's approach of firstElementChild.className to find matches. Accessed his write-up of the code on 06/24/18 at <https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/>
+/* Resources: firstElementChild.classList did not work, so used Matt Cranford's
+approach of firstElementChild.className to find matches. Accessed his write-up
+of the code on 06/24/18 at
+<https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/> */
 function checkForMatch() {
     if(flippedCards.length === 2) {
         setTimeout(function() {
@@ -72,14 +104,6 @@ function checkForMatch() {
 
 // COUNTERS
 
-// Moves (Counter)
-const moveCount = document.querySelector('.moves');
-let moves = 0;
-
-// Stars
-let stars = document.querySelectorAll('ul.stars li');
-let starList = ['star1', 'star2', 'star3'];
-
 // Counting moves and removing stars
 function addMove() {
     moves ++;
@@ -87,11 +111,12 @@ function addMove() {
     if(moves === 10) {
         hideStar();
     } else if(moves === 20) {
-        hideStar();        
+        hideStar();
     }
 };
 
-// hideStar() for...of loop courtesy of Matt Cranford's blog accessed 07/03/18, at <https://matthewcranford.com/memory-game-walkthrough-part-5-moves-stars/>
+/* hideStar() for...of loop courtesy of Matt Cranford's blog accessed 07/03/18,
+at <https://matthewcranford.com/memory-game-walkthrough-part-5-moves-stars/> */
 function hideStar() {
     for(star of stars) {
         if(star.style.visibility !== "hidden") {
@@ -100,7 +125,7 @@ function hideStar() {
             break;
         }
     }
-}
+};
 
 function resetMoves() {
     moves = 0;
@@ -118,13 +143,9 @@ function resetStars() {
 };
 
 // Clock/Timer
-// Resources: Memory Game Webinar with Ryan Waite used to help write the timer. Accessed 06/30/18, at <https://www.youtube.com/watch?v=oECVwum-7Zc&list=PL-UQ6doHcWnLwHtzKo_kGkZo9AcKI9KhL&index=12&t=2854s>. 
-const clock = document.querySelector('.clock');
-let seconds = 0;
-let minutes = 0;
-let timer;
-let timerOff = true;
-
+/* Resources: Memory Game Webinar with Ryan Waite used to help write the timer.
+Accessed 06/30/18, at
+<https://www.youtube.com/watch?v=oECVwum-7Zc&list=PL-UQ6doHcWnLwHtzKo_kGkZo9AcKI9KhL&index=12&t=2854s>. */
 function startTime() {
     timer = setInterval(function() {
         seconds ++;
@@ -156,25 +177,32 @@ function clearTime() {
     seconds = 0;
     minutes = 0;
     clock.innerHTML = `${minutes}:0${seconds}`;
-  };
+};
 
 // RESET THE GAME BOARD AND SCORES
 function resetAll() {
     flippedCards = [];
     matchedCards = [];
+    timerOff = true;
     stopTime();
     clearTime();
-    //startTime();
     resetCards();
     resetMoves();
     resetStars();
 };
 
-// MODAL
-// Resource: Used code from a Traversy Media video titled "Create a Modal with HTML, CSS, and JavaScript" viewed on 07/07/18, at <https://www.youtube.com/watch?v=6ophW7Ask_0>
+// WIN GAME FUNCTIONS
+// Check to make sure the whole deck has been matched
+function allMatched() {
+    if(matchedCards.length === 16) {
+        winGame();
+    }
+};
 
-const modal = document.querySelector('.win-modal');
-
+// Modal
+/* Resource: Used code from a Traversy Media video titled "Create a Modal with
+HTML, CSS, and JavaScript" viewed on 07/07/18, at
+<https://www.youtube.com/watch?v=6ophW7Ask_0> */
 // open modal
 function openModal() {
     modal.style.display = 'block';
@@ -192,21 +220,10 @@ function clickOutside(event) {
 };
 
 // Insert win statistics in modal:
-let winTime = document.querySelector('.clock-win');
-let winMoves = document.querySelector('.moves-win');
-let winStars = document.querySelector('.stars-win');
-
 function returnStats() {
     winTime.innerHTML = clock.innerHTML;
     winMoves.innerHTML = moves;
     winStars.innerHTML = starList.length;
-};
-
-// WIN GAME FUNCTIONS
-function allMatched() {
-    if(matchedCards.length === 16) {
-        winGame();
-    }
 };
 
 function winGame() {
@@ -216,7 +233,8 @@ function winGame() {
 };
 
 // EVENT LISTENERS
-// Resources: syntax for event listeners is from W3Schools on 06/24/18 <https://www.w3schools.com/jsref/event_onload.asp>
+/* Resources: syntax for event listeners is from W3Schools on 06/24/18
+<https://www.w3schools.com/jsref/event_onload.asp> */
 document.addEventListener('load', resetAll());
 
 resetButton.addEventListener('click', resetAll);
@@ -228,18 +246,24 @@ restartButton.addEventListener('click', function() {
 
 window.addEventListener('click', clickOutside);
 
-// the validClick function (to prevent counting clicks on the same card more than once and prevent counting clicks on cards that are already matched) is from Matt Cranford's blog, accessed 07/08/18, at <https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/>.
+/* the validClick function (to prevent counting clicks on the same card more
+than once and prevent counting clicks on cards that are already matched) is
+from Matt Cranford's blog, accessed 07/08/18, at
+<https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/>. */
 function validClick(evtTarget) {
     return(
         evtTarget.classList.contains('card') && !evtTarget.classList.contains('match') && flippedCards.length <2 && !flippedCards.includes(evtTarget)
     );
-}
+};
 
 deckOfCards.addEventListener('click', function(event) {
     let evtTarget = event.target;
     if(evtTarget.nodeName === 'LI') {
         if(validClick(evtTarget)) {
-            if(timerOff) {// function to turn the timer on with the first click is from Matt Cranford's blog, accessed 07/08/18, at <https://matthewcranford.com/memory-game-walkthrough-part-6-the-clock/>.
+            if(timerOff) {
+                /* function to turn the timer on with the first click courtesy
+                of Matt Cranford's blog, accessed 07/08/18, at
+                <https://matthewcranford.com/memory-game-walkthrough-part-6-the-clock/>. */
                 startTime();
                 timerOff = false;
             }
